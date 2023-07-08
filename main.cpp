7074fdc06b1061438a9e4d85eacdfcf39bb0d9e9
jabraham17 <jademabraham17@gmail.com>
2023-07-08 14:48:01 -0700

    regex testing
    Signed-off-by: jabraham17 <jademabraham17@gmail.com>

--- main.cpp
+++ main.cpp
@@ -53,10 +53,33 @@ int main() {
   // std::cout << s << "\n";
 
   Parser p;
-  //  (ba|c*)d
-  // auto newSl = p.parse("(((b).(a))|((c)*)).(d)",
-  //                      [](auto msg) { std::cerr << msg << "\n"; });
-  auto newSl = p.parse("(a).(b)");
+  // //  (ba|c*)d
+  // std::string regex = "(((b).(a))|((c)*)).(d)";
+  // // std::string regex = "(a)|(_)";
+  // // std::string regex = "(b).((a)*)";
+  // std::string regex = "(a)*";
+  // std::string regex = "_";
+  // std::string regex = "(a).(b)";
+  std::string regex = "(((a).(b))|(a))*"; // (ab|a)*
+  auto newSl = p.parse(regex, [](auto msg) { std::cerr << msg << "\n"; });
+
+  // StateList sl;
+  // if(0) {
+  //   auto q1 = std::make_unique<State>("q1", true);
+  //   auto q2 = std::make_unique<State>("q2");
+  //   auto q3 = std::make_unique<State>("q3");
+  //   q1->addTransition(q3.get());
+  //   q1->addTransition(q2.get(), 'b');
+  //   q2->addTransition(q2.get(), 'a');
+  //   q2->addTransition(q3.get(), 'a');
+  //   q2->addTransition(q3.get(), 'b');
+  //   q3->addTransition(q1.get(), 'a');
+  //   sl.addEntry(std::move(q1));
+  //   sl.add(std::move(q2));
+  //   sl.add(std::move(q3));
+  // }
+
+  // StateList* newSl = &sl;
 
   if(!newSl) {
     std::cerr << "error parsing regex\n";
@@ -79,25 +102,43 @@ int main() {
   pruned.close();
   system("dot -Tpng pruned.dot -o pruned.png");
 
-  system("mkdir -p testing/bin");
+  auto dfa = newSl->buildDFA();
+  std::cout << "nfa with " << newSl->states().size()
+            << " states became a dfa with " << dfa.states().size()
+            << " states\n";
 
-  auto cr = newSl->compile();
-  std::ofstream outc("testing/bin/matcher.c");
-  outc << cr.toC("match") << "\n";
-  outc.close();
+  auto g3 = dfa.toGraph("g3");
+  auto s3 = g3->toString();
+  std::ofstream dfa_g("dfa.dot");
+  dfa_g << s3 << "\n";
+  dfa_g.close();
+  system("dot -Tpng dfa.dot -o dfa.png");
 
-  system("clang -c testing/bin/matcher.c -o testing/bin/matcher_c.o");
+  if(dfa.isDFA()) {
+
+    system("mkdir -p testing/bin");
+
+    auto cr = dfa.compile();
+    std::ofstream outc("testing/bin/matcher.c");
+    outc << cr.toC("match") << "\n";
+    outc.close();
+
+    system("clang -c testing/bin/matcher.c -o testing/bin/matcher_c.o");
 
     std::ofstream outa("testing/bin/matcher.asm");
-  outa << cr.toNasm("match") << "\n";
-  outa.close();
+    outa << cr.toNasm("match") << "\n";
+    outa.close();
 
-  system("nasm -felf64 testing/bin/matcher.asm -o testing/bin/matcher_asm.o");
+    system("nasm -felf64 testing/bin/matcher.asm -o testing/bin/matcher_asm.o");
 
-
-  system("nasm -felf64 testing/inAsm.asm -o testing/bin/inAsm.o");
-  system("clang testing/test.c testing/bin/inAsm.o testing/bin/matcher_c.o -o match_c.out");
-  system("clang testing/test.c testing/bin/inAsm.o testing/bin/matcher_asm.o -o match_asm.out");
+    system("nasm -felf64 testing/inAsm.asm -o testing/bin/inAsm.o");
+    system("clang testing/test.c testing/bin/inAsm.o testing/bin/matcher_c.o "
+           "-o match_c.out");
+    system("clang testing/test.c testing/bin/inAsm.o testing/bin/matcher_asm.o "
+           "-o match_asm.out");
+  } else {
+    std::cout << "regex: '" << regex << "' is not a DFA yet\n";
+  }
 
   return 0;
 }
